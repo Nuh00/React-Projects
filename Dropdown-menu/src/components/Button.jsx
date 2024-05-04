358ccda2049021da6f90d3d29184fe1de8a6f4c8
Nuh00 <barkatnoah35@gmail.com>
2024-05-04 10:51:00 -0500

    navbar in progress

--- Dropdown-menu/src/components/Button.jsx
+++ Dropdown-menu/src/components/Button.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import "./button.css";
+import { Link } from "react-router-dom";
+
 function Button() {
-  return <div>hello from button</div>;
+  return(
+    <Link to="/sign-up" className="btn-mobile">
+      <button className="btn">Sign Up</button>
+    </Link>
+
+  );
 }
 
 export default Button;
