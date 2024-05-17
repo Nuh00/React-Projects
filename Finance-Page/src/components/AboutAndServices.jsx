a56497e23b69c918d48c3ba0c9ef74b2a3e30a42
Nuh00 <barkatnoah35@gmail.com>
2024-05-16 19:44:44 -0500

    refactoring the css

--- Finance-Page/src/components/AboutAndServices.jsx
+++ Finance-Page/src/components/AboutAndServices.jsx
@@ -10,80 +10,46 @@ function AboutAndServices() {
   return (
     <>
       <div className="wrapper">
-        <div className="two-column-container">
-          <div className="text-column">
-            <h3>Premium</h3>
-            <h2>Unlimited Transactions with zero fees</h2>
-            <p>
-              Get access to our exclusive app that allows you <br />
-              to send unlimited transactions without getting <br />
-              charged any fees.
-            </p>
-            <div className="card-button">
-              <Link to="/services" className="btn">
-                Learn More
-              </Link>
+        <div className="container">
+          <div className="cards-info">
+            <h4>PREMIUM BANK</h4>
+            <div className="information">
+              <h1>
+                Unlimited <br /> Transactions with <br /> zero fees
+              </h1>
+              <p>
+                Get access to our exclusive app that allows you to send
+                unlimited transactions without getting charged any fees.
+              </p>
+              <button>
+                <Link to="/services">Learn More</Link>
+              </button>
             </div>
           </div>
-          <div className="two-image-colum">
-            <img src={image1} alt="phone" />
-          </div>
-        </div>
-      </div>
-      <div className="wrapper light">
-        <div className="two-column-container light">
-          <div className="two-image-colum">
-            <img src={image2} alt="phone" />
-          </div>
-          <div className="text-column">
-            <h3>Unlimited Access</h3>
-            <h2>Access your account anytime, anywhere</h2>
-            <p>
-              We have you covered no matter where you are. <br />
-              All you need is an internet connection and a <br />
-              phone or computer.
-            </p>
-            <div className="card-button">
-              <Link to="/services" className="btn">
-                Learn More
-              </Link>
-            </div>
+          <div className="card-image">
+            <img src={image1} alt="image1" />
           </div>
         </div>
       </div>
 
-      <div className="wrapper">
+      <div className="wrapper-light">
         <div className="container">
-          <div className="header-column">
-            <h2>Our services</h2>
+          <div className="card-image">
+            <img src={image2} alt="image1" />
           </div>
-          <div className="three-column-container">
-            <div className="card">
-              <img src={image3} alt="phone" />
-              <h3>Secure</h3>
+          <div className="cards-info">
+            <h4>UNLIMITED ACCESS</h4>
+            <div className="information">
+              <h1>
+                Login to your <br /> account at any <br /> time
+              </h1>
               <p>
-                We value your security and privacy. <br />
-                Our app is encrypted to ensure that your <br />
-                information is safe.
+                We have you covered no matter where you are located. All you
+                need is an internet connection and a phone or computer.
               </p>
-            </div>
-            <div className="card">
-              <img src={image4} alt="phone" />
-              <h3>Easy to Use</h3>
-              <p>
-                Our app is user-friendly and easy to navigate. <br />
-                You can easily access your account and <br />
-                send transactions in seconds.
-              </p>
-            </div>
-            <div className="card">
-              <img src={image1} alt="phone" />
-              <h3>Fast</h3>
-              <p>
-                Send transactions in seconds. <br />
-                No more waiting for your transactions to <br />
-                process.
-              </p>
+              <button>
+                <Link to="/services">Learn More</Link>
+              </button>
             </div>
           </div>
         </div>
